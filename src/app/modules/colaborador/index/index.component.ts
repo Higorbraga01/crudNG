dac18416e6b7c6296b00cf2e42b35528ed3e161b
Higorbraga01 <higorbraga00@gmail.com>
2021-04-28 14:28:23 -0300

    corrigido index de colaboradores

--- src/app/modules/colaborador/index/index.component.ts
+++ src/app/modules/colaborador/index/index.component.ts
@@ -12,12 +12,6 @@ export class IndexComponent implements OnInit {
 
   productDialog: boolean;
 
-  products: any[];
-
-  product: any;
-
-  selectedProducts: any[];
-
   submitted: boolean;
 
   colaboradores: Colaborador[];
@@ -34,29 +28,6 @@ export class IndexComponent implements OnInit {
 
   }
 
-  openNew() {
-    this.product = {};
-    this.submitted = false;
-    this.productDialog = true;
-  }
-
-  deleteSelectedProducts() {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete the selected products?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.products.filter(val => !this.selectedProducts.includes(val));
-        this.selectedProducts = null;
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
-      }
-    });
-  }
-
-  editProduct(product: any) {
-    this.product = { ...product };
-    this.productDialog = true;
-  }
   remove(id): void {
     this.colaboradorService.delete(id).subscribe(() => {
       this.colaboradores.map((model, index) => {
@@ -67,63 +38,17 @@ export class IndexComponent implements OnInit {
     });
   }
 
-  deleteProduct(product: any) {
+  deleteColaborador(colaborador: Colaborador) {
     this.confirmationService.confirm({
-      message: 'Are you sure you want to delete ' + product.nome + '?',
-      header: 'Confirm',
+      message: 'Tem certeza de que deseja excluir o colaborador ' + colaborador.nome + '?',
+      header: 'Confirmação',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        this.colaboradores.filter(val => val.id !== product.id);
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
-        this.remove(product.id);
+        this.colaboradores.filter(val => val.id !== colaborador.id);
+        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Colaborador Excluido', life: 3000 });
+        this.remove(colaborador.id);
       }
     });
   }
 
-  hideDialog() {
-    this.productDialog = false;
-    this.submitted = false;
-  }
-
-  saveProduct() {
-    this.submitted = true;
-
-    if (this.product.name.trim()) {
-      if (this.product.id) {
-        this.products[this.findIndexById(this.product.id)] = this.product;
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
-      }
-      else {
-        this.product.id = this.createId();
-        this.product.image = 'product-placeholder.svg';
-        this.products.push(this.product);
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
-      }
-
-      this.products = [...this.products];
-      this.productDialog = false;
-      this.product = {};
-    }
-  }
-
-  findIndexById(id: string): number {
-    let index = -1;
-    for (let i = 0; i < this.products.length; i++) {
-      if (this.products[i].id === id) {
-        index = i;
-        break;
-      }
-    }
-
-    return index;
-  }
-
-  createId(): string {
-    let id = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (var i = 0; i < 5; i++) {
-      id += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return id;
-  }
 }
