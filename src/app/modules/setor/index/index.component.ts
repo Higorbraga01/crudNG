86948287c352a64d9e7cda9e4e1ebcb685902b57
Higorbraga01 <higorbraga00@gmail.com>
2021-04-29 13:42:56 -0300

    feat(setor): :sparkles: Implementa setor index

---
+++ src/app/modules/setor/index/index.component.ts
@@ -0,0 +1,51 @@
+import { SetorService } from './../setor.service';
+import { Setor } from './../setor.model';
+import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss']
+})
+export class IndexComponent implements OnInit {
+
+  setores: Setor[];
+
+  constructor(
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private setorService: SetorService) { }
+
+  ngOnInit() {
+    this.setorService.read().subscribe(response => {
+      this.setores = response;
+    })
+
+  }
+
+  remove(id): void {
+    this.setorService.delete(id).subscribe(() => {
+      this.setores.map((model, index) => {
+        if (id === model.id) {
+          this.setores.splice(index, 1);
+        }
+      });
+    });
+  }
+
+  deleteSetor(setor: Setor) {
+    this.confirmationService.confirm({
+      message: 'Tem certeza de que deseja excluir o setor ' + setor.descricao + '?',
+      header: 'Confirmação',
+      icon: 'pi pi-exclamation-triangle',
+      acceptButtonStyleClass: 'p-button-danger',
+      accept: () => {
+        this.setores.filter(val => val.id !== setor.id);
+        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Colaborador Excluido', life: 3000 });
+        this.remove(setor.id);
+      }
+    });
+  }
+
+}
--- src/app/modules/setor/setor.service.ts
+++ src/app/modules/setor/setor.service.ts
@@ -24,16 +24,28 @@ export class SetorService {
   //   })
   // }
 
-  create(setor: Setor): Observable<Setor> {
-    return this.http.post<Setor>(this.API_URL, setor).pipe(
-      map((obj) => obj)
-    );
+  save(data: any): Observable<Setor> {
+    return !data.id
+      ? this.http
+        .post<Setor>(this.API_URL, data)
+        .pipe(map((data) => data))
+      : this.http
+        .put<Setor>(`${this.API_URL}/${data.id}`, data)
+        .pipe(map((data) => data));
   }
 
   read(): Observable<Setor[]> {
     return this.http.get<Setor[]>(this.API_URL).pipe(
       map((obj) => obj)
     );
+  }
+
+  delete(id: number): Observable<Setor> {
+    const url = `${this.API_URL}/${id}`;
+    return this.http.delete<Setor>(url).pipe(
+      map((obj) => obj)
+    );
+
   }
 
   // errorHandler(e: any): Observable<any> {
