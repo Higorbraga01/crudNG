4b15a4925fffa7d952215938ada621ad379d8923
Higorbraga01 <higorbraga00@gmail.com>
2021-04-30 11:43:28 -0300

    feat(setor): :sparkles: Adiciona a opção de remover um colaborador direto na tabela.

--- src/app/modules/setor/index/index.component.ts
+++ src/app/modules/setor/index/index.component.ts
@@ -1,3 +1,5 @@
+import { Colaborador } from './../../colaborador/colaborador.model';
+import { ColaboradorService } from './../../colaborador/colaborador.service';
 import { SetorService } from './../setor.service';
 import { Setor } from './../setor.model';
 import { Component, OnInit } from '@angular/core';
@@ -11,11 +13,13 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 export class IndexComponent implements OnInit {
 
   setores: Setor[];
+  removido = false;
 
   constructor(
     private messageService: MessageService,
     private confirmationService: ConfirmationService,
-    private setorService: SetorService) { }
+    private setorService: SetorService,
+    private colaboradorService: ColaboradorService) { }
 
   ngOnInit() {
     this.setorService.read().subscribe(response => {
@@ -24,14 +28,21 @@ export class IndexComponent implements OnInit {
 
   }
 
-  remove(id): void {
-    this.setorService.delete(id).subscribe(() => {
-      this.setores.map((model, index) => {
-        if (id === model.id) {
-          this.setores.splice(index, 1);
+  deletarColaborador(colaborador: Colaborador) {
+    this.confirmationService.confirm({
+      message: 'Tem certeza de que deseja excluir o colaborador ' + colaborador.nome + '?',
+      header: 'Confirmação',
+      icon: 'pi pi-exclamation-triangle',
+      acceptButtonStyleClass: 'p-button-danger',
+      accept: () => {
+        this.colaboradorService.delete(colaborador.id).subscribe(() => {
+          this.removido = true;
           this.ngOnInit();
-        }
-      });
+          if (this.removido) {
+            this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Colaborador Excluido', life: 3000 });
+          }
+        });
+      }
     });
   }
 
@@ -42,9 +53,18 @@ export class IndexComponent implements OnInit {
       icon: 'pi pi-exclamation-triangle',
       acceptButtonStyleClass: 'p-button-danger',
       accept: () => {
-        this.setores.filter(val => val.id !== setor.id);
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Colaborador Excluido', life: 3000 });
-        this.remove(setor.id);
+        this.setorService.delete(setor.id).subscribe(() => {
+          this.setores.map((model, index) => {
+            if (setor.id === model.id) {
+              this.setores.splice(index, 1);
+              this.removido = true
+              this.ngOnInit();
+            }
+          });
+          if (this.removido) {
+            this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Setor Excluido', life: 3000 });
+          }
+        });
       }
     });
   }
